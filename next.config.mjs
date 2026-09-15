1952843669f368f0984bc7a29eef09c76e364c6e
whitebirdxbt <mrifqip97@gmail.com>
2026-09-15 18:09:27 +0700

    oke

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,6 @@ const nextConfig = {
       tls: false,
     };
     config.externals.push("pino-pretty", "lokijs", "encoding");
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@x402/evm/upto/client": false,
-      "@x402/evm/exact/client": false,
-      "@x402/core/client": false,
-      "@x402/svm/exact/client": false,
-      "@x402/evm": false,
-    };
     return config;
   },
 };
