ad80e3aac5bd2f38869fdbde21fc26da184eac1f
whitebirdxbt <mrifqip97@gmail.com>
2026-09-15 18:06:48 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = { ...config.resolve.fallback, fs: false, net: false, tls: false };
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@x402/evm/upto/client": false,
+      "@x402/evm/exact/client": false,
+      "@x402/core/client": false,
+      "@x402/svm/exact/client": false,
+      "@x402/evm": false,
+    };
     return config;
   },
 };
+
 export default nextConfig;
