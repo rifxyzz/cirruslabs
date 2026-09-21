3482d9e9976a5e5948f58c45a64ae0ab5171c299
whitebirdxbt <mrifqip97@gmail.com>
2026-09-21 13:52:38 +0700

    ok

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
           <div className="social-dock">
             <a
               className="x-button"
-              href="https://x.com/"
+              href="https://x.com/kerdosdex"
               target="_blank"
               rel="noreferrer"
               aria-label="KERDOS on X"
